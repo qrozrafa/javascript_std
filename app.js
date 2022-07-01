db850eb76cf615a30e029d15f5469fd6fcfabadd
qrozrafa <rafael_queirozs@hotmail.com>
2022-07-01 15:42:54 -0400

    projeto digidex finalizado, alteração nos metodos javascript, usando function await com promises, adicionado o metodo search para encontra os digimons desejados

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-const fetchDigimon = () => {
+const getDigimonUrl = `https://digimon-api.vercel.app/api/digimon` 
+const digimonsList = document.getElementById('lisDigimons')
+const searchBar = document.getElementById('searchBar');
+let digimons = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredDigimons = digimons.filter((digimon) => {
+        return (
+            digimon.name.toLowerCase().includes(searchString) ||
+            digimon.level.toLowerCase().includes(searchString)
+        );
+    });
+    console.log(filteredDigimons)
+    displayDigimons(filteredDigimons)
+});
 
 
-const getDigimonUrl = `https://digimon-api.vercel.app/api/digimon`
+const loadDigimons = async () => {
+    try{
+        const response = await fetch(getDigimonUrl);
+        digimons = await response.json();
+        displayDigimons(digimons);
+        console.log(digimons)
+    }catch (error) {
+        console.log(error)
+    }
+};
 
+const displayDigimons = (digimons) => {
+    const htmlString = digimons
+        .map((digimon) => {
+            return `
+            <li class="card">
+                <h2 class="card-title">${digimon.name}</h2>
+                <p class="card-subtitle">${digimon.level}</p>  
+                <img class="card-image" alt="${digimon.name}" src="${digimon.img}">
+            </li>
+        `;  
+    })
+    .join('');     
+    digimonsList.innerHTML = htmlString
+};
 
-    fetch(getDigimonUrl)
-        .then(response => response.json())
-        .then(digimons => {
-            const listDigimons = digimons.reduce((acc, digimon) => {
-                acc += `
-                    <li class="card">
-                        <h2 class="card-title">${digimon.name}</h2>
-                        <p class="card-subtitle">${digimon.level}</p>  
-                        <img class="card-image" alt="${digimon.name}" src="${digimon.img}">
-                    </li>`
-                return acc
-            }, '');
-
-            const ul = document.querySelector('[data-js="digidex"]')
-
-            ul.innerHTML = listDigimons
-        }) 
-        
-}
-
-
-fetchDigimon()
+loadDigimons()
