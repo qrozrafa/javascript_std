fb737376a3a4361684a0dc9a2acbd3b1b0a85374
qrozrafa <rafael_queirozs@hotmail.com>
2022-07-13 11:58:54 -0400

    obtendo valor do form e adicionando na manipulação de valores

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const transactionUl = document.querySelector('#transactions');
 const incomeDisplay = document.querySelector('#money-plus');
 const expenseDisplay = document.querySelector('#money-minus');
 const balanceDisplay = document.querySelector('#balance');
+const form = document.querySelector('#form');
+const inputTransactionName = document.querySelector('#text');
+const inputTransactionAmount = document.querySelector('#amount');
 
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
@@ -19,24 +22,28 @@ const addTransactionIntoDom = transaction => {
 
     li.classList.add(CSSClass);
     li.innerHTML= `
-        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button> 
+        ${transaction.name} 
+        <span>${operator} R$ ${amountWithoutOperator}</span>
+        <button class="delete-btn">x</button> 
     `
     transactionUl.prepend(li);
 };
 
+//manipulando os valores receitas e despesas
 const updateBalanceValues = () => {
+    //criando uma array de amounts com metodo map
     const transactionsAmounts = dummyTransactions
         .map(transaction => transaction.amount);
-
+    //somando total dos amounts
     const total = transactionsAmounts
         .reduce((accumulator, transaction) => accumulator + transaction, 0)
         .toFixed(2);
-
+    //somando as receitas
     const income = transactionsAmounts
         .filter(value => value > 0)
         .reduce((accumulator, value) => accumulator + value, 0)
         .toFixed(2);
-     
+    //somando as despesas
     const expense = Math.abs(transactionsAmounts
         .filter(value => value < 0)  
         .reduce((accumulator, value) => accumulator + value, 0))
@@ -49,8 +56,42 @@ const updateBalanceValues = () => {
 
 //preenchendo as informações mesmo com recarregamento da paginas
 const init = () => {
+    //limpando as ul para cada preenchimento
+    transactionUl.innerHTML = '';
+    //interando a transação da array, cada item dessa array sendo inserida no DOM
     dummyTransactions.forEach(addTransactionIntoDom);
     updateBalanceValues()
 }
 
 init();
+
+//gerando id
+const generateID = () => Math.round(Math.random() * 1000);
+
+//list de evento, mas nao enviando.
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    const transactionName = inputTransactionName.value.trim();
+    const transactionAmount = inputTransactionAmount.value.trim();
+
+    //verificando se o form name ou amount estão vazio
+    if (transactionName === '' || transactionAmount === '') {
+        alert('Por favor, preencha tanto o nome quanto o valor da transação.');
+        return
+    }
+
+    const transaction = { 
+        id: generateID(), 
+        name: transactionName, 
+        amount: Number(transactionAmount)
+    };
+
+    //inserido o obj transaction no dummyTransactions
+    dummyTransactions.push(transaction);
+    init();
+
+    //limpando os inputs
+    inputTransactionName.value = '';
+    inputTransactionAmount.value = '';
+});
